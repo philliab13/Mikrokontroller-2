8dcfd2969245eb59958876632ecfa605c0790b18
Linus <linus.ludescher@gmail.com>
2024-04-18 10:23:02 +0200

    ✨ feat(oppgave2): oppgavw2
    oppgave 2ferdig

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <sys/types.h>
 #include "Uart.h"
 
 void btn_init(){
@@ -27,53 +29,64 @@ void led_on(int placement){
 	GPIO0->OUTCLR &= (1<<placement);
 };
 
+ssize_t _write(int fd, const void *buf, size_t count){
+	char * letter = (char *)(buf);
+	for(int i = 0; i < count; i++){
+		uart_send(*letter);
+		letter++;
+	}
+return count;
+}
+
 
 int main(){
     led_init();
     btn_init();
     uart_init();
+	int lightAON=1;
+	int lightBON=1;
     for(int i=17; i<21;i++){
 		led_off(i);
+		lightAON=0;
 	}
     
     int sleep = 0;
     char reading = 'C';
     while(1){
-		
-		
-		
 
+		
+		
 		if (isBtnPressed(13)){
 			uart_send('A');
-			reading=uart_read();
-			
 			
 		}
 	    if (isBtnPressed(14)){
 			uart_send('B');
-			reading=uart_read();
+			
 		
 		}
+		reading=uart_read();
 
 		
         
         if(reading == 'A'){
-           led_on(17);
+           if(lightAON==0){
+			led_on(17);
+			lightAON=1;
+		   }else{led_off(17); 
+		   lightAON=0;}
+			
         }else if(reading == 'B'){
-           led_on(18);
-		}else if (reading=='\0')
-		{
-			led_on(20);
+            if(lightBON==0){
+			led_on(18);
+			lightBON=1;
+		   }else{led_off(18); 
+		   lightBON=0;}
 		}
-		
-		
-
-
-
-
 
 		sleep = 10000;
 		while(--sleep);
+		iprintf("The average grade in TTK%d was in %d was: %c\n\r",4235,2022,'B');
 
 	}
     return 0;
